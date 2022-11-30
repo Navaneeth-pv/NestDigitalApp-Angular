3f562779b1235178a99e7baa1b6b95cf62407d24
Navaneeth-pv <navaneethpv902@gmail.com>
2022-11-30 17:58:30 +0530

    data binded

--- src/app/view-all-friends/view-all-friends.component.ts
+++ src/app/view-all-friends/view-all-friends.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-view-all-friends',
@@ -6,6 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./view-all-friends.component.css']
 })
 export class ViewAllFriendsComponent {
+
+  constructor(private api:ApiService){
+
+    api.fetchFriends().subscribe(
+
+      (Response)=>{
+        this.data=Response;
+      }
+    )
+  }
 
   data:any=[]
 
