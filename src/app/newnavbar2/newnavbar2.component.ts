44390d10c62bd6c69d2bb78e859c4a8285a231c7
Navaneeth-pv <navaneethpv902@gmail.com>
2022-11-30 23:25:46 +0530

    newnabr3  added

---
+++ src/app/newnavbar2/newnavbar2.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-newnavbar2',
+  templateUrl: './newnavbar2.component.html',
+  styleUrls: ['./newnavbar2.component.css']
+})
+export class Newnavbar2Component {
+
+}
